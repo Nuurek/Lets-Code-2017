768ff04f60ef535457af584cf8c0f93f7a081ecc
Jakub Nurski <nurskijakub@gmail.com>
2017-11-18 17:19:25 +0100

    Added incrementation of Room's request counter. Added __str__ methods on models.

--- backend/officelink/camera/models.py
+++ backend/officelink/camera/models.py
@@ -8,14 +8,17 @@ class Camera(models.Model):
 
     serial_validator = RegexValidator(
         regex=r'^[0-9A-F]{16}$',
-        message="Only " + str(SERIAL_LENGTH) + " char length, hex values are allowed."
+        message="Only " + str(SERIAL_LENGTH) + " char length, hex values are allowed.",
     )
 
     serial_number = models.CharField(
         unique=True,
         max_length=SERIAL_LENGTH,
-        validators=[serial_validator]
+        validators=[serial_validator],
     )
+
+    def __str__(self):
+        return self.serial_number
 
 
 class Room(models.Model):
@@ -32,9 +35,12 @@ class Room(models.Model):
     )
 
     maximum_capacity = models.PositiveIntegerField(
-        default=1
+        default=1,
     )
 
     number_of_requests = models.PositiveIntegerField(
-        default=0
+        default=0,
     )
+
+    def __str__(self):
+        return self.name
--- backend/officelink/camera/views.py
+++ backend/officelink/camera/views.py
@@ -4,6 +4,12 @@ from .models import Room
 from .serializers import RoomSerializer
 
 
-class RoomViewSet(viewsets.ReadOnlyModelViewSet):
+class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+    def get_object(self):
+        room = super(RoomViewSet, self).get_object()
+        room.number_of_requests += 1
+        room.save()
+        return room
