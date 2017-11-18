7a475ca261b351a52222649d6f7621ab5832a7b4
Jakub Nurski <nurskijakub@gmail.com>
2017-11-18 18:06:45 +0100

    Added dj-database-url for production DB.

--- backend/officelink/officelink/settings/production.py
+++ backend/officelink/officelink/settings/production.py
@@ -1,6 +1,13 @@
+import dj_database_url
+
 from .base import *
 
 
 ALLOWED_HOSTS += (
     'officelink.herokuapp.com',
 )
+
+database_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {
+    'default': database_from_env
+}
