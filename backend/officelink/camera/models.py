9890d1bcd921bd84e7e675d4add4481c34325625
Jakub Nurski <nurskijakub@gmail.com>
2017-11-18 15:16:59 +0100

    Added Camera model.

---
+++ backend/officelink/camera/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+class Camera(models.Model):
+
+    SERIAL_LENGTH = 16
+
+    serial_validator = RegexValidator(
+        r'^[0-9A-F]{16}$',
+        "Only " + str(SERIAL_LENGTH) + " length, hex values are allowed."
+    )
+
+    serial_number = models.CharField(
+        unique=True,
+        max_length=SERIAL_LENGTH,
+        validators=[serial_validator]
+    )
